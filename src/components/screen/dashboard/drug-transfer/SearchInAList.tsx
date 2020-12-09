65052b9e245a51bf8ba136198c9f79462840c96b
rahmanism <s.mostafa.rahmani@gmail.com>
2020-12-09 12:40:29 +0330

    Resolve TF-280 - search in step 2 of exchange.

--- src/components/screen/dashboard/drug-transfer/SearchInAList.tsx
+++ src/components/screen/dashboard/drug-transfer/SearchInAList.tsx
@@ -1,13 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { DaroogSearchBar } from './DaroogSearchBar';
 import { AllPharmacyDrugInterface } from '../../../../interfaces';
 import DrugTransferContext, { TransferDrugContextInterface } from "./Context";
-import { useTranslation } from "react-i18next";
 import { Container } from "@material-ui/core";
 import { useClasses } from "../classes";
 
 const SearchInAList: React.FC = () => {
-  const { t } = useTranslation();
   const {
     container
   } = useClasses();
@@ -17,18 +15,16 @@ const SearchInAList: React.FC = () => {
   } = useContext<TransferDrugContextInterface>(DrugTransferContext);
 
   const searchHandler = (v: string) => {
-    console.log('search for', v);
-    console.log('before filter count: ', allPharmacyDrug.length);
     if (allPharmacyDrug && allPharmacyDrug.length > 0) {
       const filtered: AllPharmacyDrugInterface[] =
         allPharmacyDrug.filter((p) => {
-          // p.drug.name.includes(v) ||
-          // p.drug.companyName?.includes(v) ||
-          // p.drug.genericName?.includes(v)
+          return (
+            (p.drug.name && p.drug.name.includes(v)) ||
+            (p.drug.companyName && p.drug.companyName?.includes(v)) ||
+            (p.drug.genericName && p.drug.genericName?.includes(v))
+          )
         });
       setAllPharmacyDrug(filtered);
-      console.log('filtered array count:', filtered.length);
-      console.log('after filer count:', allPharmacyDrug.length);
     }
   }
 
