6a4366059c235622c58a2d6cdbeeef7bd1f36c48
rahmanism <s.mostafa.rahmani@gmail.com>
2021-01-16 10:58:28 +0330

    Resolve TF-563 - Add transaction

--- src/components/screen/dashboard/accounting/AddTransactionModal.tsx
+++ src/components/screen/dashboard/accounting/AddTransactionModal.tsx
@@ -1,17 +1,21 @@
 import React, { useReducer, useState } from 'react';
 import {
-  Button, Dialog, DialogActions, DialogContent,
-  DialogTitle, Divider, useMediaQuery, useTheme
+  Button, Container, Dialog, DialogActions, DialogContent,
+  DialogTitle, Divider, FormControlLabel, Grid, Paper, Radio, RadioGroup, TextField, useMediaQuery, useTheme
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { useMutation } from 'react-query';
-import { 
+import {
   AccountingTransactionInterface,
   ActionInterface
 } from '../../../../interfaces';
 import { Accounting } from '../../../../services/api';
-import { errorHandler, errorSweetAlert, successSweetAlert } from '../../../../utils';
+import {
+  errorHandler, errorSweetAlert, successSweetAlert
+} from '../../../../utils';
 import { today, todayJalali } from '../../../../utils/jalali';
+import { useClasses } from '../classes';
+import { TransactionTypeEnum } from '../../../../enum';
 
 interface Props {
   pharmacyId: number;
@@ -22,13 +26,14 @@ const initialState: AccountingTransactionInterface = {
   pharmacyId: 0,
   amount: 0,
   tarikh: todayJalali(),
-  description: ''
+  description: '',
+  transactionType: TransactionTypeEnum.Creditor,
 }
 
 function reducer(state = initialState, action: ActionInterface): any {
   const { value } = action;
 
-  switch(action.type) {
+  switch (action.type) {
     case 'pharmacyId':
       return {
         ...state,
@@ -37,7 +42,7 @@ function reducer(state = initialState, action: ActionInterface): any {
     case 'amount':
       return {
         ...state,
-        amount: value,
+        amount: value.trim(),
       };
     case 'tarikh':
       return {
@@ -47,7 +52,12 @@ function reducer(state = initialState, action: ActionInterface): any {
     case 'description':
       return {
         ...state,
-        description: value,
+        description: value.trim(),
+      };
+    case 'transactionType':
+      return {
+        ...state,
+        transactionType: value,
       };
     case 'reset':
       return initialState;
@@ -58,62 +68,138 @@ function reducer(state = initialState, action: ActionInterface): any {
 
 
 const AddTransactionModal: React.FC<Props> = ({ pharmacyId, onClose }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, { ...initialState, pharmacyId });
   const [dialogOpen, setDialogOpen] = useState(true);
   const { t } = useTranslation();
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const {
+    formContainer,
+    formItem,
+    parent,
+  } = useClasses();
+  const [showError, setShowError] = useState(false);
 
   const { add } = new Accounting();
   const [_add] = useMutation(add, {
-    onSuccess: async () => {
-      await successSweetAlert(t('alert.successfulSave'));
-      // dispatch({ type: 'reset' });
+    onSuccess: async (r) => {
+      dispatch({ type: 'reset' });
     },
+    onError: async (e) => {
+      setDialogOpen(false);
+      await errorSweetAlert(e ? e : t('alert.failed'));
+      setDialogOpen(true);
+    }
   });
 
-  const submit = async (el: React.FormEvent<HTMLFormElement>): Promise<any> => {
+  const submit = async (el: any): Promise<any> => {
     el.preventDefault();
-    // TODO: _add transcation
+
     const {
-      pharmacyId, amount, tarikh, description
+      pharmacyId, amount, tarikh, description, transactionType
     } = state;
 
-    try {
-      const result = await _add({
-        pharmacyId, amount, tarikh, description
-      });
+    if (state.amount !== 0 && state.description.length > 2) {
+      setShowError(false);
+      if (window.confirm(t('accounting.areYouSure'))) {
+        const amountToSend = transactionType == TransactionTypeEnum.Creditor
+          ? -(amount) : amount;
+        const result = await _add({
+          pharmacyId, tarikh, description, amount: amountToSend
+        });
 
-      if (result !== undefined) {
-        setDialogOpen(false);
-        if (onClose) onClose();
+        if (result !== undefined) {
+          setDialogOpen(false);
+          await successSweetAlert(result.message);
+          if (onClose) onClose();
+        }
       }
-    } catch (e) {
-      await errorSweetAlert(t('alert.failed'));
-      errorHandler(e);
+    } else {
+      setShowError(true);
     }
+  }
+
+  const addTransactionForm = (): JSX.Element => {
+    return (
+      <Container maxWidth="lg" className={ parent }>
+        <Paper>
+          <form
+            autoComplete="off"
+            className={ formContainer }
+          >
+            <Grid container spacing={ 3 }>
+              <Grid item xs={ 12 }>
+                <TextField
+                  label={ t('accounting.amount') }
+                  required
+                  error={ state.amount == 0 && showError }
+                  variant="outlined"
+                  type="number"
+                  value={ state.amount }
+                  className={ formItem }
+                  onChange={ (e): void =>
+                    dispatch({ type: 'amount', value: e.target.value })
+                  }
+                />
+              </Grid>
+              <Grid item xs={ 12 }>
+                <TextField
+                  error={ state.description.length < 3 && showError }
+                  label={ t('general.description') }
+                  required
+                  variant="outlined"
+                  value={ state.description }
+                  className={ formItem }
+                  onChange={ (e): void =>
+                    dispatch({ type: 'description', value: e.target.value })
+                  }
+                />
+              </Grid>
+              <Grid item xs={ 12 }>
+                <RadioGroup aria-label="transaction-type" name="transactionType"
+                  value={ state.transactionType } onChange={ (e): void => {
+                    dispatch({ type: 'transactionType', value: (e.target as HTMLInputElement).value })
+                  } }>
+                  <FormControlLabel
+                    value={ TransactionTypeEnum.Creditor }
+                    control={ <Radio /> }
+                    checked={ state.transactionType == TransactionTypeEnum.Creditor }
+                    label={ t('accounting.creditor') } />
+                  <FormControlLabel
+                    value={ TransactionTypeEnum.Debtor }
+                    checked={ state.transactionType == TransactionTypeEnum.Debtor }
+                    control={ <Radio /> }
+                    label={ t('accounting.debtor') } />
+                </RadioGroup>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Container>
+    )
   }
 
   return (
     <>
-      { console.log(today()) }
-      { console.log(todayJalali()) }
       <Dialog open={ dialogOpen } fullScreen={ fullScreen }>
         <DialogTitle>
           { t('accounting.addTransaction') }
         </DialogTitle>
         <Divider />
         <DialogContent>
-          <h1>Add Transaction</h1>
+          { addTransactionForm() }
         </DialogContent>
         <DialogActions>
-          {/* <Button
+          <Button
+            type="submit"
             variant="outlined"
             color="primary"
-            onClick={ submit }
+            onClick={ (e): void => {
+              submit(e);
+            } }
           >
             { t('action.save') }
-          </Button> */}
+          </Button>
           <Button
             variant="outlined"
             color="secondary"
