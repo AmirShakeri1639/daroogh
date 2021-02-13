df952f7c12091d3028258224c3adb78ca774b147
mojtaba <moj.mohammadian@gmail.com>
2021-02-13 15:53:41 +0330

    some ui changes on drawer and appbar

--- src/components/public/picture/svgIcon.tsx
+++ src/components/public/picture/svgIcon.tsx
@@ -3,14 +3,15 @@ import React, { useState, useEffect } from 'react';
 interface Props {
     fileName: string;
     className?: string;
+    size?: string 
   }
 
   const SvgIcon: React.FC<Props> = (props) => {
-    const { fileName, className = '' } = props;
+    const { fileName, className = '' ,size = "24px"} = props;
 
     return (
         <>
-          <img src={"./"+fileName+".svg"} className={ className }  style={{width:"24px" , filter:"invert(1)", transform: "scaleX(-1)"}}/>
+          <img src={"./"+fileName+".svg"} className={ className } width={size}  style= { {filter:"invert(1)", transform: "scaleX(-1)"}}/>
         </>
       );
 
