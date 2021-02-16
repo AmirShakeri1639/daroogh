b9f748d9103e058ebd3d6c72d442f177709dcffe
amir shakeri <amirsh1639@gmail.com>
2021-02-16 20:52:19 +0330

    update Map

--- src/components/public/map/MapCluster.js
+++ src/components/public/map/MapCluster.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { useState, useRef } from 'react';
 import { render } from 'react-dom';
-import MapGL, { Source, Layer, setRTLTextPlugin } from 'react-map-gl';
+import MapGL, { Source, Layer, setRTLTextPlugin, Marker } from 'react-map-gl';
+import { useQuery } from 'react-query';
+import Reports from '../../../services/api/Reports';
+import { clusterLayer } from './layers';
 
-import {
-  clusterLayer,
-  clusterCountLayer,
-  unclusteredPointLayer,
-} from './layers';
 // import './style.css';
 
 setRTLTextPlugin(
@@ -18,18 +16,24 @@ setRTLTextPlugin(
 
 const MAPBOX_TOKEN =
   'pk.eyJ1IjoiZWJpYW0iLCJhIjoiY2tkNzFuY3F2MDFtczM0azZiN3libXd1MSJ9.oIxVbKvhmqhQKJASb6iXag'; // Set your mapbox token here
-
+const { getExchangeCount } = new Reports();
 const MapCluster = () => {
   const [viewport, setViewport] = useState({
-    latitude: 40.67,
-    longitude: -103.59,
-
-    zoom: 3,
-    bearing: 0,
-    pitch: 0,
+    width: '100%',
+    height: 240,
+    latitude: 35.6892,
+    longitude: 51.389,
+    zoom: 6,
+    mapboxApiAccessToken: MAPBOX_TOKEN,
   });
+
+  const [marker] = useState({
+    latitude: 35.6892,
+    longitude: 51.389,
+  });
+  const { data } = useQuery('getExchangeCount', getExchangeCount);
   const mapRef = useRef(null);
-  
+
   const onClick = (event) => {
     debugger;
     if (event.features.length == 0) {
@@ -55,7 +59,7 @@ const MapCluster = () => {
       });
     });
   };
-
+  const size = 20;
   return (
     <>
       <MapGL
@@ -64,12 +68,45 @@ const MapCluster = () => {
         height="100%"
         mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={setViewport}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
+        ref={mapRef}
         interactiveLayerIds={[clusterLayer.id]}
         onClick={onClick}
-        ref={mapRef}
       >
-        <Source
+        {data &&
+          data &&
+          data.length &&
+          data.map((entry, index) => (
+            <div key={index}>
+              <Marker latitude={entry.item4} longitude={entry.item3}>
+                <svg
+                  overflow="visible"
+                  height={entry.item2 * 2.1}
+                  width={entry.item2 * 2.1}
+                  viewBox={'0 0 ' + entry.item2 * 2.1 + ' ' + entry.item2 * 2.1}
+                >
+                  <g>
+                    <circle
+                      cx={entry.item2}
+                      cy={entry.item2}
+                      r={entry.item2}
+                      stroke="blue"
+                      fill="blue"
+                    />
+                    <text 
+                      fontSizeAdjust="0.50"
+                      x="50%"
+                      y="50%"
+                      textAnchor="middle"
+                      fill="white"
+                    >
+                      {entry.item1}
+                    </text>
+                  </g>
+                </svg>
+              </Marker>
+            </div>
+          ))}
+        {/* <Source
           id="earthquakes"
           type="geojson"
           data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
@@ -80,7 +117,7 @@ const MapCluster = () => {
           <Layer {...clusterLayer} />
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
-        </Source>
+        </Source> */}
       </MapGL>
     </>
   );
