7bde9f4a32ab7968890f80e7607bfa86d4d946a8
rahmanism <s.mostafa.rahmani@gmail.com>
2021-02-17 00:51:24 +0330

    WIP on use blob in profile pic

--- src/components/screen/dashboard/user/changeProfilePic.tsx
+++ src/components/screen/dashboard/user/changeProfilePic.tsx
@@ -1,17 +1,23 @@
 import { User } from '../../../../services/api';
 import { errorSweetAlert, successSweetAlert } from '../../../../utils';
+import routes from '../../../../routes';
+import { File } from '../../../../services/api';
 
-const saveToStorage = (imageKey: string): void => {
+const saveToStorage = async (imageKey: string): Promise<any> => {
   const userDataFromStorage = localStorage.getItem('user');
   const userData = JSON.parse(userDataFromStorage ?? '{}');
   userData.imageKey = imageKey;
   localStorage.setItem('user', JSON.stringify(userData));
+  const fileApi = new File();
+  const blob = await fileApi.get(imageKey);
+  console.log('BLOBBBB:', blob)
+  localStorage.setItem('avatar', window.URL.createObjectURL(blob));
 }
 
 const uploadProfilePic = async (userId: number | string, file: any): Promise<any> => {
   const user = new User();
   const response = await user.changeProfileImage(userId, file);
-  saveToStorage(response.data.pictureFileKey);
+  await saveToStorage(response.data.pictureFileKey);
   return response.data.pictureFileKey;
 }
 
