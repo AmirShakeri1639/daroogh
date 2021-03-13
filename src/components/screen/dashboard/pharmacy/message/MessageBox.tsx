06d24293f1446c58821773551b308906902bf6de
Saeed Hemmati <hemmati464@gmail.com>
2021-03-13 22:10:51 +0330

    Update message list with pagination

--- src/components/screen/dashboard/pharmacy/message/MessageBox.tsx
+++ src/components/screen/dashboard/pharmacy/message/MessageBox.tsx
@@ -4,6 +4,8 @@ import { createStyles } from '@material-ui/styles';
 import { Grid, Hidden, makeStyles, Paper } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { Convertor } from 'utils';
+import { MaterialContainer } from 'components/public';
+import styled from 'styled-components';
 
 const useClasses = makeStyles((theme) =>
   createStyles({
@@ -21,6 +23,11 @@ const useClasses = makeStyles((theme) =>
   })
 );
 
+const StyledMaterialContainer = styled(MaterialContainer)`
+  display: 'flex';
+  justify-content: center !important;
+`;
+
 interface MessageBoxProps {
   message: Message;
 }
@@ -33,39 +40,51 @@ const MessageBox: React.FC<MessageBoxProps> = memo(({ message }) => {
   const { t } = useTranslation();
 
   return (
-    <Grid container spacing={3} xs={12} sm={12} md={10} lg={10} >
-      <Paper
-        style={{ padding: '16px 16px 0px 16px', marginTop: 24, width: '100%' , borderRight: '3px solid #f80501' }}
-        elevation={1}
-      >
-        <Grid item xs={12} spacing={0}>
-          <Grid container xs={12} spacing={1} >
-            <Hidden smDown>
-
-            <Grid item xs={1} alignItems="center" style={{alignSelf:"center"}} spacing={0}>
-              <img src="/message.jpg" style={{ width: '100%' }} />
-            </Grid>
-            </Hidden>
-
-            <Grid item xs={12} sm={11} spacing={0}>
-              <Grid container xs={12} spacing={0}>
-                <Grid item xs={12} spacing={1}>
-                  <strong style={{ color: '#1d0d50' }}>{subject}</strong>
+    <StyledMaterialContainer>
+      <Grid container xs={12} sm={12} md={10} lg={10} justify="center">
+        <Paper
+          style={{
+            padding: '16px 16px 0px 16px',
+            marginTop: 24,
+            width: '100%',
+            borderRight: '3px solid #f80501',
+          }}
+          elevation={1}
+        >
+          <Grid item xs={12} spacing={0}>
+            <Grid container xs={12} spacing={1}>
+              <Hidden smDown>
+                <Grid
+                  item
+                  xs={1}
+                  alignItems="center"
+                  style={{ alignSelf: 'center' }}
+                  spacing={0}
+                >
+                  <img src="/message.jpg" style={{ width: '100%' }} />
                 </Grid>
-                <Grid item xs={12} spacing={1}>
-                  <p>{message1}</p>
+              </Hidden>
+
+              <Grid item xs={12} sm={11} spacing={0}>
+                <Grid container xs={12} spacing={0}>
+                  <Grid item xs={12} spacing={1}>
+                    <strong style={{ color: '#1d0d50' }}>{subject}</strong>
+                  </Grid>
+                  <Grid item xs={12} spacing={1}>
+                    <p>{message1}</p>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item xs={12} justify="flex-end" style={{ textAlign: 'left' }}>
+            <Grid item xs={12} justify="flex-end" style={{ textAlign: 'left' }}>
               <span className="text-muted txt-xs">
                 {t('date.sendDate')}:{convertISOTime(sendDate)}
               </span>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
+        </Paper>
+      </Grid>
+    </StyledMaterialContainer>
     // <section className={boxContainer}>
     //   <div className={topContainer}>
     //     <strong>{subject}</strong>
