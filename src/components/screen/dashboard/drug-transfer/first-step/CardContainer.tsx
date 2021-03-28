ce9c81ac58412179afd4ce95a1fa2789f5561894
mojtaba <moj.mohammadian@gmail.com>
2021-03-28 23:04:09 +0430

    font style changed

--- src/components/screen/dashboard/drug-transfer/first-step/CardContainer.tsx
+++ src/components/screen/dashboard/drug-transfer/first-step/CardContainer.tsx
@@ -152,7 +152,7 @@ const CardContainer: React.FC<CardContainerRelatedPharmacyDrugsInterface> = (
           itemsCount={itemsCount}
           userType={userType}
         />
-        <span  style={{fontSize:12 , marginBottom:4}}>نمونه اقلام عرضه شده در این داروخانه:</span>
+        <span  style={{fontSize:11 , color:'GrayText' , marginBottom:4}}>نمونه اقلام عرضه شده در این داروخانه:</span>
 
         <Box component="div" className={box}>
           <Grid container spacing={0}>
