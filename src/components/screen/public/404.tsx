1cb0dcce63a551e784f85eed65f584b2ab83c77e
rahmanism <s.mostafa.rahmani@gmail.com>
2021-04-01 02:22:37 +0430

    Resolve TF-861 - Add 404 page

--- src/components/screen/public/404.tsx
+++ src/components/screen/public/404.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import 'assets/scss/404.scss';
 
 const Error404: React.FC = () => {
   const { t } = useTranslation()
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-      <h1>{ t('error._404') }</h1>
+    <div className="site">
+      <div className="sketch">
+        <div className="bee-sketch red"></div>
+        <div className="bee-sketch blue"></div>
+      </div>
+
+      <h1>
+        404:
+        <small>{ t('error._404') }</small>
+        <p>{ t('error._404_desc') }</p>
+        <button className="btn">{ t('general.home') }</button>
+      </h1>
     </div>
   )
 }
