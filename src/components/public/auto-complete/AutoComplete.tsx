dfcd01e702a32c41500bed214a61a399b87078a2
Saeed Hemmati <hemmati.464@gmail.com>
2021-04-01 16:28:42 +0000

    Feature/autocomplete

--- src/components/public/auto-complete/AutoComplete.tsx
+++ src/components/public/auto-complete/AutoComplete.tsx
@@ -7,15 +7,12 @@ import React, {
   useState,
 } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faTimes,
-  faCaretDown,
-  faCaretUp,
-} from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import style from './auto-complete.module.scss';
 import { useTranslation } from 'react-i18next';
 import { isUndefined, indexOf } from 'lodash';
 import SelectedItems from './SelectedItems';
+import styled from 'styled-components';
 
 export interface ListOptions {
   label: string;
@@ -33,6 +30,14 @@ interface Props {
   defaultSelectedItem?: string;
   multiple?: boolean;
 }
+
+const Li = styled.li`
+  &:focus,
+  &.focus {
+    background-color: rgb(235, 235, 235);
+    transition: background-color 0.2s ease;
+  }
+`;
 
 const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
   (
@@ -52,30 +57,70 @@ const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
     const [inputValue, setInputValue] = useState<string>('');
     const [showOptionsList, setShowOptionsList] = useState(false);
     const [valuesArray, setValuesArray] = useState<ListOptions[]>([]);
+    const [liActiveIndex, setLiActiveIndex] = useState(0);
+    const [prevOffsetTop, setPrevOffsetTop] = useState(0);
 
     useEffect(() => {
-      const escapeHandler = (e: KeyboardEvent): void => {
-        if (e.key === 'Escape') {
-          setShowOptionsList(false);
+      const keyHandler = (e: KeyboardEvent): void => {
+        const ulEl = (document.querySelectorAll('#ac-li-items li') as unknown) as HTMLElement[];
+        const setClass = (index: number): void => ulEl[index].classList.add('focus');
+        const removeClass = (index: number): void => ulEl[index].classList.remove('focus');
+
+        if (showOptionsList) {
+          switch (e.key) {
+            case 'Escape':
+              setShowOptionsList(false);
+              break;
+            case 'ArrowDown':
+              removeClass(liActiveIndex);
+              const index = liActiveIndex < ulEl.length - 1 ? liActiveIndex + 1 : ulEl.length - 1;
+              setLiActiveIndex(index);
+              setClass(index);
+              break;
+            case 'ArrowUp':
+              removeClass(liActiveIndex);
+              const _index = liActiveIndex > 0 ? liActiveIndex - 1 : 0;
+              setLiActiveIndex(_index);
+              setClass(_index);
+              break;
+            case 'Enter':
+              ulEl[liActiveIndex].click();
+              break;
+          }
         }
       };
 
-      document.addEventListener('keydown', escapeHandler);
+      document.addEventListener('keydown', keyHandler);
       return (): void => {
-        document.removeEventListener('keydown', escapeHandler);
+        document.removeEventListener('keydown', keyHandler);
       };
-    }, []);
+    }, [options, liActiveIndex, showOptionsList]);
+
+    useEffect(() => {
+      const focusedEl = (document.querySelector('.focus') as unknown) as HTMLElement;
+      const optionsContainer = (document.getElementById('div-list') as unknown) as HTMLElement;
+      if (focusedEl) {
+        setPrevOffsetTop(focusedEl.offsetTop);
+        if (
+          prevOffsetTop < focusedEl.offsetTop &&
+          focusedEl.offsetHeight + focusedEl.offsetTop > focusedEl!.offsetParent!.clientHeight
+        ) {
+          optionsContainer.scrollTop = focusedEl.offsetHeight + optionsContainer.scrollTop;
+        } else if (prevOffsetTop > focusedEl.offsetTop) {
+          optionsContainer.scrollTop = focusedEl.offsetTop - focusedEl.offsetHeight;
+        }
+      }
+    }, [liActiveIndex]);
 
     useEffect(() => {
       const wrapperEl = document.getElementById('wrapper') as HTMLDivElement;
-      const dropdownEl =
-        (document.getElementById('div-list') as HTMLDivElement) ?? null;
+      const dropdownEl = (document.getElementById('div-list') as HTMLDivElement) ?? null;
 
       const handler = (e: MouseEvent): void => {
-        const isClickIsideEl = wrapperEl.contains(e.target as any);
+        const isClickInSideEl = wrapperEl.contains(e.target as any);
         if (dropdownEl !== null) {
           const isClickIsidedDropDown = dropdownEl?.contains(e.target as any);
-          if (!isClickIsideEl && !isClickIsidedDropDown) {
+          if (!isClickInSideEl && !isClickIsidedDropDown) {
             setShowOptionsList(false);
           }
         }
@@ -98,32 +143,31 @@ const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
 
     const optionItems = useCallback(() => {
       if (options) {
-        return options
-          .filter((_item: any) => indexOf(valuesArray, _item.item) === -1)
-          .map((option: any) => {
-            return (
-              <li
-                key={option.item.value}
-                value={option.item.value}
-                className={
-                  isInArray(option.item.value) ? style['active-item'] : ''
-                }
-                onClick={(): void => {
-                  if (multiple && indexOf(valuesArray, option.item) === -1) {
-                    setValuesArray((v) => [...v, option.item]);
-                    onItemSelected([...valuesArray, option.item]);
-                    setInputValue('');
-                  } else if (!multiple) {
-                    onItemSelected([option.item]);
-                    setInputValue(option.item.label);
-                    setShowOptionsList(false);
-                  }
-                }}
-              >
-                {option.el}
-              </li>
-            );
-          });
+        return React.Children.toArray(
+          options
+            .filter((_item: any) => indexOf(valuesArray, _item.item) === -1)
+            .map((option: any) => {
+              return (
+                <Li
+                  value={option.item.value}
+                  className={isInArray(option.item.value) ? style['active-item'] : ''}
+                  onClick={(): void => {
+                    if (multiple && indexOf(valuesArray, option.item) === -1) {
+                      setValuesArray((v) => [...v, option.item]);
+                      onItemSelected([...valuesArray, option.item]);
+                      setInputValue('');
+                    } else if (!multiple) {
+                      onItemSelected([option.item]);
+                      setInputValue(option.item.label);
+                      setShowOptionsList(false);
+                    }
+                  }}
+                >
+                  {option.el}
+                </Li>
+              );
+            })
+        );
       }
     }, [options, valuesArray]);
 
@@ -136,9 +180,7 @@ const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
     };
 
     const arrayItemRemoveHandler = (item: ListOptions): void => {
-      const filteredItems = valuesArray.filter(
-        (_item) => _item.value !== item.value
-      );
+      const filteredItems = valuesArray.filter((_item) => _item.value !== item.value);
       setValuesArray([...filteredItems]);
       onItemSelected([...filteredItems]);
     };
@@ -147,16 +189,11 @@ const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
       <div id="wrapper" className={style['wrapper']}>
         <div
           className={`${style['input-container']} ${
-            options?.length === 0 || !showOptionsList
-              ? style['itself-space']
-              : ''
+            options?.length === 0 || !showOptionsList ? style['itself-space'] : ''
           }`}
           onFocus={(): void => setShowOptionsList(true)}
         >
-          <SelectedItems
-            items={valuesArray}
-            removeHandler={arrayItemRemoveHandler}
-          />
+          <SelectedItems items={valuesArray} removeHandler={arrayItemRemoveHandler} />
 
           <input
             type="text"
@@ -197,7 +234,7 @@ const AutoComplete: React.FC<Props & { ref: Ref<any> }> = forwardRef(
             {isLoading ? (
               <span className="text-muted">{loadingText}</span>
             ) : !isUndefined(options) && options.length > 0 ? (
-              <ul>{optionItems()}</ul>
+              <ul id="ac-li-items">{optionItems()}</ul>
             ) : (
               <span>{t('general.noData')}</span>
             )}
