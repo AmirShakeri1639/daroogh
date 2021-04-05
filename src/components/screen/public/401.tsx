4da0a01e482041d7929453979157ff32a31071da
rahmanism <s.mostafa.rahmani@gmail.com>
2021-04-05 23:37:11 +0430

    Resolve TF-902 - Add 401 error page

--- src/components/screen/public/401.tsx
+++ src/components/screen/public/401.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import 'assets/scss/404.scss';
+import 'assets/scss/401.scss';
 import { Link } from 'react-router-dom';
 
 const Error401: React.FC = () => {
   const { t } = useTranslation()
 
   return (
-    <div>
+    <div className="body-error">
+      <div className="error-logo">
+        <img src='/images/401.jpg' alt="ERROR 401" />
+      </div>
       <h1>
-        <small>{ t('error._401') }</small>
-        <p>{ t('error._401_desc') }</p>
-        <Link to="/">
-          <button className="btn">{ t('general.home') }</button>
-        </Link>
+        { t('error._401') }
       </h1>
+      <p>{ t('error._401_desc') }</p>
+      <div>
+        <Link to="/">
+          <button className="btn btn-purple">{ t('general.home') }</button>
+        </Link>
+      </div>
     </div>
-    // <div className="body-404">
-    //   <div className="site">
-    //     <h1>
-    //     <small>{ t('error._401') }</small>
-    //       <p>{ t('error._401_desc') }</p>
-    //       <Link to="/">
-    //         <button className="btn">{ t('general.home') }</button>
-    //       </Link>
-    //     </h1>
-    //   </div>
-    // </div>
   )
 }
 
