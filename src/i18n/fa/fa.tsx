f910e2059d7ffe84e887985b097c2c6a0ae36659
mojtaba <moj.mohammadian@gmail.com>
2021-04-09 10:53:38 +0430

    typo changes

--- src/i18n/fa/fa.tsx
+++ src/i18n/fa/fa.tsx
@@ -467,7 +467,7 @@ export default {
         // 9
         CONFIRMA_AND_B_PAYMENTB: 'در انتظار پرداخت (طرف اول هستید)',
         // 10
-        CONFIRMALL_AND_PAYMENTALL: 'تبادل کامل شده',
+        CONFIRMALL_AND_PAYMENTALL: 'تبادل انجام شده',
         // 11
         NOSEND_FORB: 'ارسال نشده',
         // 12
@@ -485,7 +485,7 @@ export default {
         // 18
         CONFIRMA_AND_B_PAYMENTA_FORB: 'در انتظار پرداخت (طرف دوم هستید)',
         // 19 - 'تائید طرفین و در انتظار پرداخت طرف مقابل',
-        CONFIRMA_AND_B_PAYMENTB_FORB: 'تبادل کامل شده',
+        CONFIRMA_AND_B_PAYMENTB_FORB: 'تبادل انجام شده',
         // 20
         CONFIRMALL_AND_PAYMENTALL_FORB: 'تبادل کامل شده',
       },
