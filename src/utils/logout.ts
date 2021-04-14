ff12b505db274159c410fb47ad5b1bded6a1588c
Rahmanism <s.mostafa.rahmani@gmail.com>
2021-04-14 19:25:58 +0430

    Remove debugger

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -4,7 +4,6 @@ export default (): void => {
   if (window.confirm('برای خروج اطمینان دارید؟')) {
     const impersonation = new Impersonation();
     if (impersonation.currentToken === impersonation.mainToken) {
-      debugger
       const version = localStorage.getItem('version')
       localStorage.clear();
       if (version) localStorage.setItem('version', version)
