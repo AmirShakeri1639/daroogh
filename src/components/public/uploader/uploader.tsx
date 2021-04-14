6e02d13aabd12d45b8d45765d70cf10e7d876bde
Rahmanism <s.mostafa.rahmani@gmail.com>
2021-04-14 10:42:43 +0430

    Add onDelete to uploader

--- src/components/public/uploader/uploader.tsx
+++ src/components/public/uploader/uploader.tsx
@@ -24,53 +24,58 @@ interface UploaderPI {
   showSaveClick?: boolean;
   getFile: (e: any) => void;
   handleOnSave?: (e: any) => void;
+  onDelete?: () => void;
 }
+
 const Uploader = (props: UploaderPI): JSX.Element => {
-  const { getFile, handleOnSave, showSaveClick = false } = props;
+  const {
+    getFile, handleOnSave, showSaveClick = false,
+    onDelete
+  } = props;
   const { input, ulStyle } = useStyle();
   const [file, setFile] = React.useState<any>();
   const [fileName, setFileName] = React.useState<string>();
   return (
-    <ul className={ulStyle}>
+    <ul className={ ulStyle }>
       <li>
         <img
-          src={file ? file : noImage}
-          width={50}
-          height={50}
-          style={{
+          src={ file ? file : noImage }
+          width={ 50 }
+          height={ 50 }
+          style={ {
             border: '1px solid silver',
             borderRadius: 5,
             marginTop: 6,
             marginRight: 5,
-          }}
+          } }
         ></img>
       </li>
       <li>
         <TextField
-          style={{
+          style={ {
             marginTop: -2,
             marginBottom: 5,
             marginRight: 10,
             fontSize: 10,
             minWidth: 160,
-          }}
+          } }
           size="small"
           defaultValue="فایلی انتخاب نشده است"
-          value={fileName}
-          inputProps={{
+          value={ fileName }
+          inputProps={ {
             readOnly: true,
             style: { fontSize: 10, textAlign: 'center' },
-          }}
+          } }
         />
       </li>
       <li>
         <input
           accept="image/*"
-          className={input}
+          className={ input }
           id="contained-button-file"
           multiple
           type="file"
-          onChange={(e): void => {
+          onChange={ (e): void => {
             if (e.target.files) {
               let reader = new FileReader();
               reader.onload = (e) => {
@@ -81,7 +86,7 @@ const Uploader = (props: UploaderPI): JSX.Element => {
               setFileName(e.target.files[0].name);
               e.target.value = '';
             }
-          }}
+          } }
         />
         <label htmlFor="contained-button-file">
           <Button
@@ -89,37 +94,38 @@ const Uploader = (props: UploaderPI): JSX.Element => {
             color="primary"
             component="span"
             size="small"
-            style={{ marginRight: 10 }}
+            style={ { marginRight: 10 } }
           >
             دریافت فایل
           </Button>
         </label>
         <Button
-          style={{ marginRight: 10 }}
+          style={ { marginRight: 10 } }
           variant="contained"
           color="secondary"
           component="span"
           size="small"
-          onClick={() => {
+          onClick={ () => {
             setFile(null);
             setFileName('فایلی انتخاب نشده است');
-          }}
+            if (onDelete) onDelete()
+          } }
         >
           حذف
         </Button>
-        {showSaveClick && (
+        { showSaveClick && (
           <Button
-            style={{ marginRight: 10, backgroundColor: 'green', color: 'white' }}
+            style={ { marginRight: 10, backgroundColor: 'green', color: 'white' } }
             variant="contained"
             component="span"
             size="small"
-            onClick={() => {
+            onClick={ () => {
               if (handleOnSave) handleOnSave(file);
-            }}
+            } }
           >
             ذخیره
           </Button>
-        )}
+        ) }
       </li>
     </ul>
   );
