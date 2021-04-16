5d2b38c67f3cc500bc2ea75028a0a448e45b298d
Saeed Hemmati <hemmati.464@gmail.com>
2021-04-16 10:59:15 +0000

    Fix/beta

--- src/components/screen/dashboard/pack/create/Create.tsx
+++ src/components/screen/dashboard/pack/create/Create.tsx
@@ -280,7 +280,7 @@ const Create: React.FC = () => {
         if (el.scrollTop === scrollHeight) {
           clearInterval(interval);
         }
-      });
+      }, 50);
     }
   }, [comissionPercent, daroogRecommendation]);
 
