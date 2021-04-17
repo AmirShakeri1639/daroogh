6ef7a4b2e1254a75d344e639bd875e5e28d8afa4
mojtaba <moj.mohammadian@gmail.com>
2021-04-17 10:00:25 +0430

    some style changes on pharmacy docs

--- src/components/public/card-container/Details.tsx
+++ src/components/public/card-container/Details.tsx
@@ -6,61 +6,66 @@ import noImage from 'assets/images/no-image.png';
 import { File } from 'services/api';
 import { api } from 'config/default.json';
 import { DataTableColumns } from 'interfaces/DataTableColumns';
+import { ColorEnum } from 'enum';
 
 interface Props {
-  data: DataTableColumns[]
+  data: DataTableColumns[];
 }
 
 const Detail: React.FC<Props> = (props) => {
-  const { data } = props
-  const { t } = useTranslation()
-  const { urls } = new File()
+  const { data } = props;
+  const { t } = useTranslation();
+  const { urls } = new File();
 
   const addDefaultSrc = (ev: any): void => {
     ev.target.src = noImage;
     ev.target.onerror = null;
   };
 
-  const [fileKey, setFileKey] = useState('')
+  const [fileKey, setFileKey] = useState('');
   useEffect(() => {
-    const fileKey = data.find(i => i.field == 'fileKey' && i.value)
-    setFileKey(fileKey?.value ?? '')
-  }, [data])
+    const fileKey = data.find((i) => i.field == 'fileKey' && i.value);
+    setFileKey(fileKey?.value ?? '');
+  }, [data]);
 
   return (
     <>
-      <Grid item xs={ 12 } spacing={ 0 }>
-        <Grid container spacing={ 2 }>
-          { fileKey &&
-            <Grid item style={ { textAlign: 'center' } } xs={ 4 }>
+      <Grid item xs={12} spacing={0} style={{ paddingTop: 8 }}>
+        <Grid container spacing={2}>
+          {fileKey && (
+            <Grid item style={{ textAlign: 'center' }} xs={4}>
               <img
-                onError={ addDefaultSrc }
-                style={ { height: '80px', width: '80px', margin: '5px' } }
-              src={ `${api.baseUrl}${urls.get}?key=${fileKey}` }
+                onError={addDefaultSrc}
+                style={{ height: '80px', width: '80px', margin: '5px' }}
+                src={`${api.baseUrl}${urls.get}?key=${fileKey}`}
               />
             </Grid>
-          }
-          <Grid item xs={ 8 }>
-            <Grid container item spacing={ 1 }>
-              {
-                data.map((i: any): any => {
-                  if (i.hidden) return
-                  return (
-                    <Grid item xs={ 12 }>
-                      <TextWithTitle
-                        title={ i.title }
-                        body={ i.value }
-                      />
-                    </Grid>
-                  )
-                })
-              }
+          )}
+          <Grid
+            item
+            xs={8}
+            style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}
+          >
+            <Grid
+              container
+              item
+              spacing={1}
+              style={{ borderRight: `2px solid ${ColorEnum.Borders}`, paddingRight: 8 }}
+            >
+              {data.map((i: any): any => {
+                if (i.hidden) return;
+                return (
+                  <Grid item xs={12}>
+                    <TextWithTitle title={i.title} body={i.value} />
+                  </Grid>
+                );
+              })}
             </Grid>
           </Grid>
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
 export default Detail;
