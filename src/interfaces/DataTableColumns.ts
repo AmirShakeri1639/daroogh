753731ea7f21a023d9e610af829c6ec6be6f5fbc
Rahmanism <s.mostafa.rahmani@gmail.com>
2021-04-17 05:27:43 +0430

    Add DataTableColumns to card container and details

--- src/interfaces/DataTableColumns.ts
+++ src/interfaces/DataTableColumns.ts
@@ -12,6 +12,9 @@ export interface DataTableColumns {
   render?: any;
   lookupFilter?: LookupFilter[];
   lookup?: any;
+  hidden?: boolean;
+  // if you want to send data (value) with structure in some cases
+  value?: any;
   filterComponent?: (props: {
     columnDef: Column<any>;
     onFilterChanged: (rowId: string, value: any) => void;
