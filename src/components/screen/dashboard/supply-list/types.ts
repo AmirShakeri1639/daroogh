18c99c9bb87ee617fab1b09b893d3b552ecf5895
Saeed Hemmati <hemmati.464@gmail.com>
2021-04-23 13:16:43 +0000

    Dev

---
+++ src/components/screen/dashboard/supply-list/types.ts
@@ -0,0 +1,6 @@
+export enum FilterItems {
+  MAXIMUM_INVENTORY = 'maximumInventory',
+  MINIMUM_INVENTORY = 'minimumInventory',
+  NEAREST_EXPIRE_DATE = 'nearestExpireDate',
+  FARTHEST_EXPIRE_DATE = 'farthestExpireDate'
+};
