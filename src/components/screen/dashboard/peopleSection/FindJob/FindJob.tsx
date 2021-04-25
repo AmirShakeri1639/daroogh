281bcac269e73d701eabb2ec36619f2f93bcbaaf
mojtaba <moj.mohammadian@gmail.com>
2021-04-25 13:11:44 +0430

    add countryDivition to FindJob

--- src/components/screen/dashboard/peopleSection/FindJob/FindJob.tsx
+++ src/components/screen/dashboard/peopleSection/FindJob/FindJob.tsx
@@ -1,42 +1,104 @@
-import { Container, Grid } from '@material-ui/core'
+import { Container, createStyles, Grid, makeStyles } from '@material-ui/core'
+import { County, Province } from 'components/public'
 import TextWithTitle from 'components/public/TextWithTitle/TextWithTitle'
 import React, { useState, useEffect, useRef, useMemo, Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FindJob as Job } from 'services/api'
-import { FindJobInterface , finJobDetailInterface } from '../../../../../interfaces'
+import { FindJobInterface, finJobDetailInterface } from '../../../../../interfaces'
 import CardContainer from './CardContainer'
 
 const FindJob: React.FC = () => {
   const [dataList, setDataList] = useState<FindJobInterface>()
 
   const [countryDivisionCode, setCountryDivisionCode] = useState<string>('')
+  const useStyle = makeStyles((theme) =>
+    createStyles({
+      input: {
+        width: '80%',
+      },
+    })
+  )
 
+  const { input } = useStyle()
+  const [selectedCounty, setSelectedCounty] = useState<string>('*')
+  const [selectedProvince, setSelectedProvince] = useState<string>('-2')
   useEffect(() => {
     async function getData() {
+      var countryDivision = ''
+      if (selectedProvince !== '-2' && selectedProvince !== '*') {
+        countryDivision = String(selectedProvince);
+      } else if (selectedCounty !== '-2') {
+        countryDivision = String(selectedCounty);
+      }
+
       const { all } = new Job()
-      const result = await all('09')
+      const result = await all(countryDivision)
 
       setDataList(result)
     }
     getData()
-  }, [])
+  }, [selectedProvince,selectedCounty])
+
+
+  const { t } = useTranslation()
 
   return (
     <Container>
-    <Grid container spacing={3}>
+      <Grid container spacing={3} style={{ marginTop: 16 }}>
+        <Grid item container xs={12}>
+          <Grid
+            item
+            container
+            xs={12}
+            sm={6}
+            style={{
+              alignItems: 'center',
+              alignContent: 'center',
+              display: 'flex',
+              marginBottom: 8,
+            }}
+          >
+            <Grid item xs={12}>
+              <County
+                className={input}
+                countyHandler={(e): void => {
+                  setSelectedCounty(e ?? '')
+                  setSelectedProvince('-2')
+                }}
+                value={selectedCounty}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            style={{ alignItems: 'center', alignContent: 'center', display: 'flex',              marginBottom: 8,
+          }}
+          >
+            <Grid item xs={12}>
+              <Province
+                className={input}
+                countyId={selectedCounty}
+                value={selectedProvince}
+                provinceHandler={(e): void => setSelectedProvince(e ?? '')}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
 
-      {dataList !== undefined && (
-        <>
-          {dataList.items.map((job: finJobDetailInterface): any => {
-            return (
-              <Grid item xs={12} sm={6} md={3} lg={4}>
-                <CardContainer job={job}/>
-              </Grid>
-            )
-          })}
-        </>
-      )}
-    </Grid>
+        {dataList !== undefined && (
+          <>
+            {dataList.items.map((job: finJobDetailInterface): any => {
+              return (
+                <Grid item xs={12} sm={6} md={3} lg={4}>
+                  <CardContainer job={job} />
+                </Grid>
+              )
+            })}
+          </>
+        )}
+      </Grid>
     </Container>
   )
 }
