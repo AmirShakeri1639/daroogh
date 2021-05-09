6d5db11a4a8adae86918d7c49dfab3cdff675036
amir shakeri <amirsh1639@gmail.com>
2021-05-10 00:20:33 +0430

    update

--- src/components/public/map/Map.tsx
+++ src/components/public/map/Map.tsx
@@ -1,22 +1,22 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 'react-leaflet'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 'react-leaflet';
 
-import L, { LatLngExpression } from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import SearchControl from './SearchControl'
-L.Icon.Default.imagePath = 'images/'
+import L, { LatLngExpression } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import SearchControl from './SearchControl';
+L.Icon.Default.imagePath = 'images/';
 
 interface Props {
-  onClick?: (e: any) => void
-  defaultLatLng?: [number, number]
-  maxHeight?: string
-  editable?: boolean
-  draggable: boolean
-  getGeoLocation?: boolean
-  hasGeocoder?: boolean
+  onClick?: (e: any) => void;
+  defaultLatLng?: [number, number];
+  maxHeight?: string;
+  editable?: boolean;
+  draggable: boolean;
+  getGeoLocation?: boolean;
+  hasGeocoder?: boolean;
 }
 interface MarkerProps {
-  defaultLatLng?: [number, number]
+  defaultLatLng?: [number, number];
 }
 
 const Map: React.FC<Props> = (props) => {
@@ -28,13 +28,13 @@ const Map: React.FC<Props> = (props) => {
     editable = false,
     getGeoLocation = false,
     hasGeocoder = false,
-  } = props
+  } = props;
 
-  const [position, setPosition] = useState(defaultLatLng)
-  const [draggableRes, setDraggable] = useState(draggable)
+  const [position, setPosition] = useState(defaultLatLng);
+  const [draggableRes, setDraggable] = useState(draggable);
   const onSearchSelect = (e: any) => {
-    setPosition([e.y, e.x])
-  }
+    setPosition([e.y, e.x]);
+  };
 
   return (
     <div style={{ position: 'relative' }}>
@@ -62,19 +62,20 @@ const Map: React.FC<Props> = (props) => {
         />
       </MapContainer>
     </div>
-  )
-}
+  );
+};
 
 interface MarkerProps {
-  onClick?: (e: any) => void
-  defaultLatLng?: [number, number]
-  maxHeight?: string
-  editable: boolean
-  draggable: boolean
-  getGeoLocation?: boolean
-  hasGeocoder?: boolean
+  onClick?: (e: any) => void;
+  defaultLatLng?: [number, number];
+  maxHeight?: string;
+  editable: boolean;
+  draggable: boolean;
+  getGeoLocation?: boolean;
+  hasGeocoder?: boolean;
 }
 const AddMarkerToClick: React.FC<MarkerProps> = (props) => {
+  debugger;
   const {
     onClick,
     maxHeight = '400px',
@@ -83,31 +84,31 @@ const AddMarkerToClick: React.FC<MarkerProps> = (props) => {
     editable,
     getGeoLocation = false,
     hasGeocoder = false,
-  } = props
+  } = props;
   const [position, setPosition] = useState({
     latitude: defaultLatLng[0],
     longitude: defaultLatLng[1],
-  })
+  });
   const map = useMapEvents({
     click(event) {
-      const { lat, lng } = event.latlng
+      const { lat, lng } = event.latlng;
       setPosition({
         latitude: lat,
         longitude: lng,
-      })
+      });
     },
-  })
+  });
   useEffect(() => {
     setPosition({
       latitude: defaultLatLng[0],
       longitude: defaultLatLng[1],
-    })
-    map.setView([defaultLatLng[0], defaultLatLng[1]], map.getZoom())
-  }, [defaultLatLng[0], defaultLatLng[1]])
+    });
+    map.setView([defaultLatLng[0], defaultLatLng[1]], map.getZoom());
+  }, [defaultLatLng[0], defaultLatLng[1]]);
 
   return position.latitude ? (
     <Marker position={[position.latitude, position.longitude]} draggable={draggable && editable} />
-  ) : null
-}
+  ) : null;
+};
 
-export default Map
+export default Map;
