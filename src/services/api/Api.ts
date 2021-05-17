e19661e851a3309a7b81292b7cfb8e25f3a1cccd
Rahmanism <s.mostafa.rahmani@gmail.com>
2021-05-17 11:20:27 +0430

    Fix error interceptors in axios

--- src/services/api/Api.ts
+++ src/services/api/Api.ts
@@ -4,8 +4,6 @@ import { errorHandler, tError } from 'utils'
 import i18n from 'i18n'
 import { ToastDurationEnum } from 'utils/toast'
 import { getBaseUrl } from 'config';
-import moment from 'jalali-moment';
-import Utils from 'components/public/utility/Utils'
 
 const axiosInstance = axios.create({
   baseURL: getBaseUrl(),
@@ -60,67 +58,6 @@ axiosInstance.interceptors.request.use(config => {
   return Promise.reject(error);
 });
 */
-
-axiosInstance.interceptors.response.use(undefined, (error) => {
-  const { response } = error
-
-  if (!error.response) {
-    tError(i18n.t('error.network'), {
-      autoClose: ToastDurationEnum.VeryLong,
-      position: 'top-center',
-      toastId: ErrorToastId.NETWORK
-    })
-    console.error('Error in network')
-    return Promise.reject(error)
-  }
-
-  if (error.response.data?.message) {
-    console.error(error.response.data.message)
-  }
-
-  const { status } = response
-  if (status === 400) {
-    ; (async (): Promise<any> => {
-      const message = error?.response?.data?.message
-      tError(
-        message === undefined
-          ? 'مشکلی در ارسال درخواست وجود دارد،' +
-          ' در صورت تکرار لطفا با پشتیبانی تماس بگیرید!'
-          : message,
-        { toastId: ErrorToastId.ERR_400 }
-      )
-    })()
-  } else if (status === 401) {
-    ; (async (): Promise<any> => {
-      const message = error?.response?.data?.message
-      tError(
-        message === undefined
-          ? 'شما مجوز دسترسی به این صفحه را ندارید!'
-          : message,
-        { toastId: ErrorToastId.ERR_401 }
-      )
-    })()
-  } else if (status === 404) {
-    ; (async (): Promise<any> => {
-      tError(
-        'پیدا نشد! - 404',
-        { toastId: ErrorToastId.ERR_404 }
-      )
-    })()
-  } else if (status === 500) {
-    ; (async (): Promise<any> => {
-      const message = error?.response?.data?.message
-      tError(
-        message === undefined ? 'خطایی رخ داده است.' : message,
-        { toastId: ErrorToastId.ERR_500 }
-      )
-    })()
-  }
-
-  return Promise.reject(
-    error?.response?.data?.message == undefined ? error : error?.response?.data?.message
-  )
-})
 
 class Api {
   protected axiosInstance: AxiosInstance = axiosInstance
